c68422bed6a9fbc38444c70ba8fe4dde78e2bc98
Shivam Raina <sraina753@gmail.com>
2022-05-16 15:26:13 +0530

    fixing routing

--- index.js
+++ index.js
@@ -50,7 +50,10 @@ app.post('/api/bookhostel', async(req, res) => {
 })
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+	});
 }
 
 const port = process.env.PORT || 5000;
